0ed375316ab4f75ea16fc7fd2e27c98684659a4e
Marcelo Marcus Nogueira Martins <marcelo@cglam.local>
2020-07-16 10:49:06 -0400

    css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-window.alert("Olá Mundo 2")
+//window.alert("Olá Mundo 2")
+//window.console.log("Estou no console")
+
+//Pegar um documento do HTML pelo ID
+//window.document.getElementById("titulo")
+
+//Para EXIBIR o elemento dentro do console
+window.console.log(
+    window.document.getElementById("titulo").innerHTML
+    )
+
