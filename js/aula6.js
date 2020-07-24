7a29b94947b18a31ffcef13b57c8870214549759
Marcelo Marcus Nogueira Martins <marcelo@cglam.local>
2020-07-24 12:19:23 -0400

    sexta-feira

---
+++ js/aula6.js
@@ -0,0 +1,12 @@
+console.log(
+    document.getElementById("p1").innerHTML
+)
+
+document.getElementById("p1").innerHTML = "Novo texto inserido"
+
+
+var nome = document.getElementById("p2")
+nome.innerHTML = "Dev: Marcelo Martins"
+
+nome.style.color = "red"
+nome.style.backgroundColor = "#ddd"
