615a5188e665ac870dfb5b712de277eb8429e949
Marcelo Marcus Nogueira Martins <marcelo@cglam.local>
2020-07-24 10:10:48 -0400

    JS

--- js/aula4.js
+++ js/aula4.js
@@ -1,3 +1,36 @@
 console.log(
-    
+
+    document.getElementsByTagName("div")["0"].innerHTML
+   
+
 )
+
+console.log(
+
+    document.getElementsByTagName("br")
+  
+
+)
+
+//querySelector retorna a primeira ocorrência
+console.log (
+
+    document.querySelector("div")
+
+)
+
+//querySelector se for pegar pela classe
+console.log (
+
+    document.querySelector(".bloco2")
+
+)
+
+//querySelector se for retornar pelo id
+console.log (
+
+    document.querySelector("#par2")
+
+)
+
+
---
+++ js/aula5.js
@@ -0,0 +1,7 @@
+console.log(
+    document.querySelectorAll(".times")
+)
+
+console.log(
+    document.getElementById("futebol-europeu").querySelectorAll(".times")[0].innerHTML
+)
