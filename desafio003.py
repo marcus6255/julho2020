0c3b7373e09c4c3d543a0290b31695c0baeeeb1c
Marcelo Marcus Nogueira Martins <marcelo@cglam.local>
2020-07-29 11:55:05 -0400

    treino

---
+++ desafio003.py
@@ -0,0 +1,5 @@
+v1 = int(input("Digite um valor: "))
+v2 = int(input("Digite outro valor: "))
+soma = v1+v2
+
+print ("A soma de {} com {} é igual a {}. ".format(v1,v2,soma))
---
+++ desafio004.py
@@ -0,0 +1,13 @@
+digite = input("Digite alguma coisa: ")
+print("A palavra que você digitou é ", type(digite))
+print("Somente espaços: ", digite.isspace())
+print("É número:", digite.isnumeric())
+print("É alfabético: ", digite.isalpha())
+print ("É alfanu´merico", digite.isalnum())
+print("Está em maiúscula: ", digite.isupper())
+print("Está em minúscula: ", digite.islower())
+
+
+
+
+
